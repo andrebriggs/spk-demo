ca9462137be5d92967aa2d3510b5e14ffab08862
Dennis Seah <dennis.seah@gmail.com>
2020-02-05 09:46:01 -0800

    cleanup

--- src/constant_values.ts
+++ src/constant_values.ts
@@ -1,11 +1,11 @@
-export const AZDO_ORG = "abrig"
-export const AZDO_ORG_URL = "https://dev.azure.com/abrig"
-export const AZDO_PROJECT = "spk"
+export const AZDO_ORG = "abrig";
+export const AZDO_ORG_URL = "https://dev.azure.com/abrig";
+export const AZDO_PROJECT = "spk";
 export const MANIFEST_REPO = "quick-start-manifest";
 export const HLD_REPO = "quick-start-hld";
 export const APP_REPO = "quick-start-app";
-export const ACCESS_TOKEN = "rmsajzlsff3x34e64dmc63c2agrrvdzq3hcwpi7j5do3q7j3o3ca"
+export const ACCESS_TOKEN = "rmsajzlsff3x34e64dmc63c2agrrvdzq3hcwpi7j5do3q7j3o3ca";
 export const DIRECTORY_LIST: string[] = [MANIFEST_REPO, HLD_REPO, APP_REPO];
-export const USER = "service_account"
+export const USER = "service_account";
 export const WORKSPACE = "test-env";
 
