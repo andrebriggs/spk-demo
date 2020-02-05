5855cbadd96a8321a3f862b38a75456e5ccc0b6a
Dennis Seah <dennis.seah@gmail.com>
2020-02-05 10:02:44 -0800

    tslint fixes

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,33 @@
-import clear from "clear"
-import * as constants from "./constant_values";
-import rimraf from "rimraf";
-import chalk from "chalk"
-import cli  from "clui"
-import inquirer from 'inquirer'
-import { exec } from "../../spk/src/lib/shell";
-
-import { initialize as hldInitialize} from "../../spk/src/commands/hld/init";
-import fs from "fs-extra";
-import * as azOps from "./az_utils";
-
-import path from "path";
-import simplegit, { StatusResult, SimpleGit } from "simple-git/promise"
-import { IGitApi } from "azure-devops-node-api/GitApi";
-import * as vsoNodeApi from "azure-devops-node-api";
+import * as GitInterfaces from "azure-devops-node-api/interfaces/GitInterfaces";
 import { GitRepository } from "azure-devops-node-api/interfaces/TfvcInterfaces";
 
-import * as GitInterfaces from "azure-devops-node-api/interfaces/GitInterfaces";
-import { logger } from "../../spk/src/logger";
-import { hasValue } from "../../spk/src/lib/validator";
+import chalk from "chalk";
+import child_process from "child_process";
+import clear from "clear";
+import cli from "clui";
+import fs from "fs-extra";
+import inquirer from "inquirer";
+import os from "os";
+import path from "path";
+import rimraf from "rimraf";
+import simplegit, { SimpleGit, StatusResult  } from "simple-git/promise";
 
-const Spinner = cli.Spinner
-const spawn = require('child_process').spawn
+import { initialize as hldInitialize} from "../../spk/src/commands/hld/init";
+import { exec } from "../../spk/src/lib/shell";
+
+import { hasValue } from "../../spk/src/lib/validator";
+import { logger } from "../../spk/src/logger";
+import * as azOps from "./az_utils";
+import * as constants from "./constant_values";
+
+const spawn = child_process.spawn;
+const Spinner = cli.Spinner;
 
 const init = async () => {
   clear();
   console.log(
     chalk.blueBright("Welcome SPK Quick Start!")
-  );   
+  );
 };
 
 const installHelper = (
@@ -36,11 +36,11 @@ const installHelper = (
   spinner: cli.Spinner
 ) => {
   return new Promise((resolve, reject) => {
-    const process = spawn(command, { shell: true })
-    spinner.start()
+    const process = spawn(command, { shell: true });
+    spinner.start();
     process.on('exit', () => {
-      spinner.stop()
-      onSuccess()
+      spinner.stop();
+      onSuccess();
       resolve();
     });
   });
@@ -51,12 +51,12 @@ const installPromiseHelper = (
   onSuccess: { (): void; (): void },
   spinner: cli.Spinner
 ) => {
-  return new Promise(async (resolve, reject) => {      
-    spinner.start()
+  return new Promise(async (resolve, reject) => {
+    spinner.start();
     await delegate.then(() => {
-      spinner.stop()
-      onSuccess()
-      resolve()
+      spinner.stop();
+      onSuccess();
+      resolve();
     });
   });
 };
@@ -65,10 +65,10 @@ const installSpinner = async (installSubject: string) => {
   const spinner = new Spinner(`Installing ${installSubject}`);
   return installHelper(
     // 'yarn add -D prettier',
-    'sleep 5',
+    "sleep 5",
     () => console.log(chalk.green(`Done installing ${installSubject}`)),
     spinner
-  )
+  );
 };
 
 const installPromiseSpinner = async (
@@ -104,12 +104,12 @@ const installPromiseSpinner = async (
 
 const askOrganization = () => {
   const questions = [{
+    message: 'Enter {organization name}\n',
     name: 'azdo_org_name',
     type: 'input',
-    message: 'Enter {organization name}\n',
-    validate: function (value: string) {
-      var pass = value.match(
-        /^\S*$/ //No Spaces
+    validate: (value: string) => {
+      const pass = value.match(
+        /^\S*$/ // No Spaces
       );
       if (pass) {
         return true;
@@ -117,9 +117,9 @@ const askOrganization = () => {
       return 'Organization names must start with a letter or number, followed by letters, numbers or hyphens, and must end with a letter or number.';
     }
   }, {
+    message: 'Enter {project name}\n',
     name: 'azdo_project_name',
     type: 'input',
-    message: 'Enter {project name}\n'
     // validate: function(value: string) {
     //     var pass = value.match(
     //         /[^A-Za-z0-9-]+/ //See https://www.regextester.com
@@ -131,19 +131,19 @@ const askOrganization = () => {
     //     return 'Project names must start with a letter or number, followed by letters, percent, numbers or hyphens, and must end with a letter or number.';
     //   }
   }, {
-    type: 'password',
+    mask: '*',
     message: 'Enter your AzDO personal access token',
     name: 'azdo_pat',
-    mask: '*',
+    type: 'password',
     validate: (value: string) => {
       if (!hasValue(value)) {
-        return 'Must enter a personal access token with read/write/manage permissions'
+        return 'Must enter a personal access token with read/write/manage permissions';
       }
       return true;
     }
   }];
 
-  return inquirer.prompt(questions)
+  return inquirer.prompt(questions);
 };
 
 export const pushBranch = async (branchName: string): Promise<void> => {
@@ -158,12 +158,12 @@ export const pushBranch = async (branchName: string): Promise<void> => {
 // let hldRepoFiles: string[] = ["README.md"];
 // let appRepoFiles: string[] = ["README.md"];
 
-function logCurrentDirectory() {
+const logCurrentDirectory = () => {
   logger.info(`Current directory: ${process.cwd()}`);
-}
+};
 
 const createDirectory = (dirName: string) => {
-  fs.mkdirpSync(dirName)
+  fs.mkdirpSync(dirName);
 };
 
 const moveToRelativePath = (relativePath: string) => {
@@ -174,8 +174,8 @@ const moveToAbsPath = (absPath: string) => {
   process.chdir(absPath);
 };
 
-const homedir = require('os').homedir();
-const WORKSPACE_DIR = path.resolve(path.join(homedir,constants.WORKSPACE));
+const homedir = os.homedir();
+const WORKSPACE_DIR = path.resolve(path.join(homedir, constants.WORKSPACE));
 
 const createRepoInAzureOrg = async (
   azureOrgUrl: string,
@@ -183,12 +183,12 @@ const createRepoInAzureOrg = async (
   repoName: string,
   projectName: string
 ): Promise<GitRepository> => {
-  const vstsCollectionLevel = await azOps.getWebApi(azureOrgUrl,accessToken); //org url
+  const vstsCollectionLevel = await azOps.getWebApi(azureOrgUrl,accessToken); // org url
   const gitApi = await vstsCollectionLevel.getGitApi();
-  const createOptions = <GitInterfaces.GitRepositoryCreateOptions>{
+  const createOptions = {
     name: repoName,
     project: projectName
-  };
+  } as GitInterfaces.GitRepositoryCreateOptions;
   return await gitApi.createRepository(createOptions, projectName);
 };
 
@@ -196,10 +196,10 @@ const logGitInformation = (gitStatus: StatusResult) => {
   logger.info("Files in local Git:");
   gitStatus.files.forEach(f => logger.info("\t" + f.path));
 
-  logger.info("Files added to Git:")
+  logger.info("Files added to Git:");
   gitStatus.created.forEach(f => logger.info("\t" + f));
 
-  logger.info(chalk.yellow("Files not added to Git:"))
+  logger.info(chalk.yellow("Files not added to Git:"));
   gitStatus.not_added.forEach(f => logger.info("\t" + f));
 };
 
@@ -210,77 +210,77 @@ const commitAndPushToRemote = async (
   accessToken: string,
   repoName: string
 ) => {
-  //Commit and check the local git log
-  await git.commit(`Initial commit for ${repoName} repo`)
+  // Commit and check the local git log
+  await git.commit(`Initial commit for ${repoName} repo`);
 
-  var resultLog = await git.log()
-  logger.info("Log Messages from Git:")
+  const resultLog = await git.log();
+  logger.info("Log Messages from Git:");
   resultLog.all.forEach(f => logger.info("\t" + f.date + " --> " + f.message));
 
   // TOFIX: We know AzDO url style so hack it for now instead of discovering via API
   const remoteURL =
-    `dev.azure.com/${azureOrgName}/${azureProjectName}/_git/${repoName}`
+    `dev.azure.com/${azureOrgName}/${azureProjectName}/_git/${repoName}`;
 
   const remote = `https://${constants.USER}:${accessToken}@${remoteURL}`;
 
   // Push to remote
-  await git.addRemote('origin', remote)
+  await git.addRemote('origin', remote);
   await git.push('origin', 'master');
 
-  logger.info(`Finished pushing ${repoName} repo!`)
+  logger.info(`Finished pushing ${repoName} repo!`);
 };
 
 const scaffoldManifestRepo = async () => {
-    //Set up Manifest Repo
+  // Set up Manifest Repo
   try {
-    const currentRepo = constants.MANIFEST_REPO
-    const azureOrgUrl = constants.AZDO_ORG_URL
-    const azureOrgName = constants.AZDO_ORG
-    const azureProjectName = constants.AZDO_PROJECT
-    const accessToken = constants.ACCESS_TOKEN
+    const currentRepo = constants.MANIFEST_REPO;
+    const azureOrgUrl = constants.AZDO_ORG_URL;
+    const azureOrgName = constants.AZDO_ORG;
+    const azureProjectName = constants.AZDO_PROJECT;
+    const accessToken = constants.ACCESS_TOKEN;
 
-    moveToAbsPath(WORKSPACE_DIR)
-    createDirectory(currentRepo)
-    moveToRelativePath(currentRepo)
+    moveToAbsPath(WORKSPACE_DIR);
+    createDirectory(currentRepo);
+    moveToRelativePath(currentRepo);
 
     const gitRepo = await azOps.findRepoInAzureOrg(azureOrgUrl, accessToken, currentRepo);
 
     if (gitRepo.id) {
       await azOps.deleteRepoInAzureOrg(azureOrgUrl, accessToken, gitRepo, azureProjectName);
     }
-    const resultRepo = await createRepoInAzureOrg(azureOrgUrl, accessToken, currentRepo, azureProjectName)
+    const resultRepo = await createRepoInAzureOrg(azureOrgUrl, accessToken, currentRepo, azureProjectName);
     logger.info("Result repo: " + resultRepo.remoteUrl);
 
-    logCurrentDirectory()
+    logCurrentDirectory();
     const git = simplegit();
     if (!await git.checkIsRepo()) {
-      await git.init()
-      logger.info(`Git init called in ${process.cwd()}`)
+      await git.init();
+      logger.info(`Git init called in ${process.cwd()}`);
     }
 
     // Create and add files
     fs.createFileSync("README.md");
-    await git.add("./README.md")
+    await git.add("./README.md");
 
-    await commitAndPushToRemote(git, azureOrgName, azureProjectName, accessToken, currentRepo)
+    await commitAndPushToRemote(git, azureOrgName, azureProjectName, accessToken, currentRepo);
   } catch (err) {
-    //TODO err displays access token
-    logger.error(`An error occured: ${err}`)
+    // TODO err displays access token
+    logger.error(`An error occured: ${err}`);
   }
 };
 
 const scaffoldHLDRepo = async () => {
-  //Set up HLD Repo
+  // Set up HLD Repo
   try {
-    const currentRepo = constants.HLD_REPO
-    const azureOrgUrl = constants.AZDO_ORG_URL
-    const azureOrgName = constants.AZDO_ORG
-    const azureProjectName = constants.AZDO_PROJECT
-    const accessToken = constants.ACCESS_TOKEN
+    const currentRepo = constants.HLD_REPO;
+    const azureOrgUrl = constants.AZDO_ORG_URL;
+    const azureOrgName = constants.AZDO_ORG;
+    const azureProjectName = constants.AZDO_PROJECT;
+    const accessToken = constants.ACCESS_TOKEN;
 
-    moveToAbsPath(WORKSPACE_DIR)
-    createDirectory(currentRepo)
-    moveToRelativePath(currentRepo)
+    moveToAbsPath(WORKSPACE_DIR);
+    createDirectory(currentRepo);
+    moveToRelativePath(currentRepo);
 
     const gitRepo = await azOps.findRepoInAzureOrg(azureOrgUrl, accessToken, currentRepo);
 
@@ -288,25 +288,25 @@ const scaffoldHLDRepo = async () => {
       await azOps.deleteRepoInAzureOrg(azureOrgUrl, accessToken, gitRepo, azureProjectName);
     }
 
-    const resultRepo = await createRepoInAzureOrg(azureOrgUrl, accessToken, currentRepo, azureProjectName)
+    const resultRepo = await createRepoInAzureOrg(azureOrgUrl, accessToken, currentRepo, azureProjectName);
     logger.info("Result repo: " + resultRepo.remoteUrl);
 
-    logCurrentDirectory()
+    logCurrentDirectory();
     const git = simplegit();
     if (!await git.checkIsRepo()) {
-      await git.init()
-      logger.info(`Git init called in ${process.cwd()}`)
+      await git.init();
+      logger.info(`Git init called in ${process.cwd()}`);
     }
 
     // Create and add files
     await hldInitialize(".", false);
-    await git.add("./*")
-    logGitInformation(await git.status())
+    await git.add("./*");
+    logGitInformation(await git.status());
 
-    await commitAndPushToRemote(git, azureOrgName, azureProjectName, accessToken, currentRepo)
+    await commitAndPushToRemote(git, azureOrgName, azureProjectName, accessToken, currentRepo);
   } catch (err) {
-    //TODO err displays access token
-    logger.error(`An error occured: ${err}`)
+    // TODO err displays access token
+    logger.error(`An error occured: ${err}`);
   }
 };
 
@@ -321,10 +321,10 @@ const scaffoldHLDRepo = async () => {
   const answer = await askOrganization();
   console.log("Org Name:\t" + answer.azdo_org_name);
   console.log("Project Name:\t" + answer.azdo_project_name);
-  console.log("https://dev.azure.com/" + answer.azdo_org_name + "/" + answer.azdo_project_name)
-  //TODO: link the inputs
+  console.log("https://dev.azure.com/" + answer.azdo_org_name + "/" + answer.azdo_project_name);
+  // TODO: link the inputs
 
-  logger.info(`The WORKSPACE_DIR is ${WORKSPACE_DIR}`)
+  logger.info(`The WORKSPACE_DIR is ${WORKSPACE_DIR}`);
   if (fs.existsSync(WORKSPACE_DIR)) {
     rimraf.sync(WORKSPACE_DIR);
   }
@@ -341,7 +341,7 @@ const scaffoldHLDRepo = async () => {
   // answer.then(answers => {
   //     console.log("Org Name:\t"+answers.azdo_org_name);
   //     console.log("Project Name:\t"+answers.azdo_project_name);
-  //     console.log("https://dev.azure.com/"+answers.azdo_org_name+"/"+answers.azdo_project_name)      
+  //     console.log("https://dev.azure.com/"+answers.azdo_org_name+"/"+answers.azdo_project_name)
   // });
 
   // console.log(chalk.yellow("You chose wisely 🧙‍♂️"))
